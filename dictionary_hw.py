bed9c8a1ad5192e7298e42dc6b2551848353101b
AGR1234567 <ridgeutube@gmail.com>
2024-10-30 07:40:34 +0000

    Add files via upload

---
+++ dictionary_hw.py
@@ -0,0 +1,5 @@
+books = {'Maths':'130 INR', 'English':'210 INR', 'Biology':'190 INR', 'Chemistry':'140 INR', 'Physics':'100 INR', 'History':'160 INR', 'Geography':'120 INR', 'French':'250 INR', 'Spanish':'240 INR'}
+books['Physics'] = '200 INR'
+books['TP'] = '270 INR'
+books['Music'] = '160 INR'
+print(books)
